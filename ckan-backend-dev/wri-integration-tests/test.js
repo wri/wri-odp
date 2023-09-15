22fe7be6cb8dfc2369f1c8982370f6d3336690c9
Michael Polidori <contact@michaelpolidori.com>
2023-09-15 13:47:46 -0400

    Revert "Add exit code output for GitHub Actions"
    This reverts commit 09c334098731f197969972292c370a3379410157.

--- ckan-backend-dev/wri-integration-tests/test.js
+++ ckan-backend-dev/wri-integration-tests/test.js
@@ -6,20 +6,6 @@ assets.addSpecs(".", ["ckan-classic-auth", "ckan-classic-api"]);
 
 cypress
   .run(assets.options)
-  .then(r => {
-    console.log(r)
-    assets.cleanUp()
-
-    // if the cypress tests fail to run, log the error
-    if (r.failures) {
-      console.error('Could not execute cypress tests')
-      console.error(r.message)
-      process.exit(1)
-    }
-    // return exit code of 1 if any tests fail
-    r.totalFailed ? process.exit(1) : process.exit(0)
-  })
-  .catch(e => {
-    console.error(e.message)
-    process.exit(1)
-  });
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => assets.cleanUp());
