3b70faa83e99070169f3f97fd35441a4b025570c
Michael Polidori <contact@michaelpolidori.com>
2023-09-20 10:05:20 -0400

    Fix s3filestore script shebang

--- ckan-backend-dev/ckan/scripts/fix_s3filestore_test_ini.sh
+++ ckan-backend-dev/ckan/scripts/fix_s3filestore_test_ini.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 sed -i "s|ckanext.s3filestore.aws_access_key_id = test-access-key|ckanext.s3filestore.aws_access_key_id = ${AWS_ACCESS_KEY_ID}|g" src/ckanext-s3filestore/test.ini
 sed -i "s|ckanext.s3filestore.aws_secret_access_key = test-secret-key|ckanext.s3filestore.aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}|g" src/ckanext-s3filestore/test.ini
