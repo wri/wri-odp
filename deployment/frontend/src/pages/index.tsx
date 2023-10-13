a537e879075ab0bd66bdac9c2c553b812532df0d
Luccas Mateus <luccasmmg@gmail.com>
2023-10-13 13:38:12 -0300

    Changing text color

--- deployment/frontend/src/pages/index.tsx
+++ deployment/frontend/src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <h1 className="text-green-600 text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             WRI ODP
           </h1>
           <div className="flex flex-col items-center gap-2">
