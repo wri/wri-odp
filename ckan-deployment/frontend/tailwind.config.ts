8b0c661ebaf4b7d08582ddebebdeb8a79ca62ebc
steveoni <steohenoni2@gmail.com>
2023-10-15 18:46:28 +0100

    [tailwind.config][s]: move customize object to extend

--- ckan-deployment/frontend/tailwind.config.ts
+++ ckan-deployment/frontend/tailwind.config.ts
@@ -4,18 +4,22 @@ import defaultTheme from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      white: "#FFFFFF",
-      "wri-gold": "#F3B229",
-      "wri-black": "#1A1919",
-      "wri-green": "#32864B",
-      "wri-gray": "#eae8e4"
-    },
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         acumin: ['"Acumin Pro"', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        white: "#FFFFFF",
+        "wri-gold": "#F3B229",
+        "wri-black": "#1A1919",
+        "wri-green": "#32864B",
+        "wri-gray": "#eae8e4"
+      },
+      screens: {
+        "4xl": "2048px",
+        "3xl": "1920px",
+      }
     },
   },
   plugins: [],
