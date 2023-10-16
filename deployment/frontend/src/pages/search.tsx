8327e06142cbcbd64df5d43799331630c8355c08
steveoni <steohenoni2@gmail.com>
2023-10-16 13:12:14 +0100

    [Search][m]: implement search page

---
+++ deployment/frontend/src/pages/search.tsx
@@ -0,0 +1,19 @@
+import Search from '@/components/Search'
+import Header from '@/components/_shared/Header'
+import Highlights from '@/components/Highlights'
+import Recent from '@/components/Recent'
+import Footer from '@/components/_shared/Footer'
+
+
+
+export default function test() {
+  return (
+    <div className="">
+      <Header />
+      <Search />
+      <Highlights />
+      <Recent />
+      <Footer />
+    </div>
+  )
+}
