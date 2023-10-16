d7e793c8e39f50d6c3812d50b336c4938f88802d
steveoni <steohenoni2@gmail.com>
2023-10-16 13:09:16 +0100

    [taildwind.config][m]: add custom themes

--- deployment/frontend/tailwind.config.ts
+++ deployment/frontend/tailwind.config.ts
@@ -1,9 +1,27 @@
 import { type Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        acumin: ['"Acumin Pro"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        white: "#FFFFFF",
+        "wri-gold": "#F3B229",
+        "wri-black": "#1A1919",
+        "wri-green": "#32864B",
+        "wri-gray": "#eae8e4",
+        "wri-dark-green": "#2B7340"
+      },
+      screens: {
+        "4xl": "2048px",
+        "3xl": "1920px",
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
