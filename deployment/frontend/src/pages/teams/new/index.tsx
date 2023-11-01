52c54b9d0d26abbdfe97a46455b0f301225f9d25
Leonardo Yuri Farias <57202549+Gutts-n@users.noreply.github.com>
2023-11-01 11:37:35 -0300

    Created new team and new topic pages (#65)
    * Created new team and new topic pages
    
    * Adjusts in imports and labels names
    
    * Added bottom border and shadow to cards
    
    * Added color to bottom border of Upload button
    
    * Adjust in pages URLs property
    
    * Minor tweaks
    
    * Decrease padding
    
    ---------
    
    Co-authored-by: João Demenech <joao.demenech@datopian.com>
    Co-authored-by: leonardo.farias <leonardo.farias@wedotec.com.br>
    Co-authored-by: Luccas Mateus <luccasmmg@gmail.com>

---
+++ deployment/frontend/src/pages/teams/new/index.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/_shared/Header";
+import CreateTeam from "@/components/team/CreateTeam";
+
+export default function TeamsPage() {
+
+  return (
+    <>
+      <Header />
+      <CreateTeam/>
+    </>
+  );
+}
