bd090ba98acc3fdbca69be392324a1da476b6929
leonardo.farias <leonardo.farias@wedotec.com.br>
2023-11-01 00:09:00 -0300

    Created new team and new topic pages

--- deployment/frontend/src/components/_shared/InputGroup.tsx
+++ deployment/frontend/src/components/_shared/InputGroup.tsx
@@ -3,22 +3,29 @@ import classNames from "@/utils/classnames";
 export function InputGroup({
   label,
   className,
+  labelClassName,
   children,
   required = false,
 }: {
   label: string;
   className?: string;
+  labelClassName?: string,
   children: React.ReactNode;
   required?: boolean;
 }) {
   return (
     <div
       className={classNames(
-        "flex items-center justify-between gap-x-14",
+        "md:flex items-center justify-between gap-x-14",
         className ?? "",
       )}
     >
-      <span className="max-w-[4.6rem] text-left font-acumin text-lg font-normal leading-tight text-black">
+      <span
+        className={classNames(
+          "w-[4rem] text-left font-acumin text-lg font-normal leading-tight text-black",
+          labelClassName ?? ""
+        )}
+      >
         {label} {required && <span className="text-red-500">*</span>}
       </span>
       {children}
---
+++ deployment/frontend/src/components/topics/CreateTopic.tsx
@@ -0,0 +1,95 @@
+import Container from "../_shared/Container";
+import { InputGroup } from "../_shared/InputGroup";
+import Select from "../_shared/SimpleSelect";
+import { Input } from "../_shared/SimpleInput";
+import { UploadButton } from "../datasets/sections/datafiles/Upload";
+import { Breadcrumbs } from "../_shared/Breadcrumbs";
+
+const links = [
+  { label: "Topics", url: "/topics", current: false },
+  { label: "Create a topic", url: "/topics/new", current: true },
+];
+
+export function CreateTopic() {
+  return (
+    <>
+      <Breadcrumbs links={links} />
+      <Container className="mb-20 font-acumin">
+        <h1 className="mb-[2rem] text-[1.57rem] font-semibold">
+          Create a topic
+        </h1>
+        <div className="w-full border-b">
+          <div className="px-8">
+            <div className="grid grid-cols-1 items-start gap-x-24 gap-y-4 py-5 md:grid-cols-2">
+              <div className="flex flex-col justify-start gap-y-4">
+                <InputGroup label="Name" required>
+                  <Input
+                    name="name"
+                    maxWidth="100%"
+                    placeholder="Name"
+                    type="text"
+                  />
+                </InputGroup>
+                <InputGroup label="Title" required>
+                  <Input
+                    name="title"
+                    maxWidth="100%"
+                    placeholder="My dataset"
+                    type="text"
+                  />
+                </InputGroup>
+                <InputGroup label="URL" required>
+                  <Input
+                    name="url"
+                    maxWidth="100%"
+                    placeholder="name-of-dataset"
+                    type="text"
+                    className="!pl-[4.8rem]"
+                  >
+                    <span className="absolute inset-y-0 left-5 flex items-center pr-3 sm:text-sm sm:leading-6">
+                      /dataset/
+                    </span>
+                  </Input>
+                </InputGroup>
+                <InputGroup label="Image" className="justify-start gap-x-[2.7rem]" required>
+                  <div className="col-span-full lg:col-span-2">
+                    <div className="w-[11rem]">
+                      <UploadButton text="Upload an image" />
+                    </div>
+                  </div>
+                </InputGroup>
+              </div>
+              <div className="flex flex-col justify-start gap-y-4">
+                <InputGroup
+                  label="Description"
+                  labelClassName="pt-[0.9rem]"
+                  className="items-start"
+                >
+                  <Input
+                    maxWidth="100%"
+                    placeholder="Description"
+                    name="Description"
+                    type="text"
+                    as="textarea"
+                    className="h-[8.4rem]"
+                  />
+                </InputGroup>
+                <InputGroup
+                  label="Description"
+                  labelClassName="pt-[0.9rem]"
+                  className="items-start"
+                >
+                  <Select
+                    maxWidth="100%"
+                    options={[{ label: "Parent 1", value: "PARENT_1" }]}
+                    placeholder="Select a parent"
+                  />
+                </InputGroup>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
