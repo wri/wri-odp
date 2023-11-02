232ebffa4291fdfcb9f3b1e60c28f046dd17679f
steveoni <steohenoni2@gmail.com>
2023-11-02 14:18:11 +0100

    add aproval modal

--- deployment/frontend/src/pages/datasets/[datasetName].tsx
+++ deployment/frontend/src/pages/datasets/[datasetName].tsx
@@ -44,6 +44,7 @@ export default function DatasetPage() {
         <>
             <Header />
             <Breadcrumbs links={links} />
+            {isApprovalRequest && (<ApprovalRequestCard />)}
             <DatasetPageLayout
                 lhs={
                     isAddLayers ? (
