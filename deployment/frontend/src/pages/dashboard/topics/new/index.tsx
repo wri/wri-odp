a6d98247a408fc6ca04083ed0358a52fdb01e3b2
Luccas Mateus <Luccasmmg@gmail.com>
2023-11-06 12:47:10 -0300

    Fix links (#72)
    * Fix links
    
    * [Login][s]: change emal address to username and make password, password type
    
    * fix: footer links
    
    * Revert "fix: footer links"
    
    This reverts commit ed90332b5be32ae97bdc1a2b3f67bb540a2a8832.
    
    ---------
    
    Co-authored-by: steveoni <steohenoni2@gmail.com>
    Co-authored-by: Demenech <joaommdemenech@gmail.com>

--- deployment/frontend/src/pages/dashboard/topics/new/index.tsx
+++ deployment/frontend/src/pages/dashboard/topics/new/index.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/_shared/Header";
 import { CreateTopic } from "@/components/topics/CreateTopic";
 
-export default function TeamsPage() {
+export default function NewTopicsPage() {
 
   return (
     <>
