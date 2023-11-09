53c20c4fb44b7ab079beced2b89bae728751f1a4
Demenech <joaommdemenech@gmail.com>
2023-11-09 14:50:33 -0300

    fix: add missing env var to tests

--- ckan-backend-dev/ckan/scripts/cypress_setup.sh
+++ ckan-backend-dev/ckan/scripts/cypress_setup.sh
@@ -18,3 +18,5 @@ cp ../../../integration-tests/cypress.json.example ../../../integration-tests/cy
 # Replace the token placeholder with the generated token
 sed -i "s/CKAN_API_TOKEN/$token/g" ../../../integration-tests/cypress.json
 sed -i "s/CKAN_API_TOKEN/$token/g" ../../../e2e-tests/cypress.config.js
+
+docker exec -i ckan-wri /bin/bash -c "export SYS_ADMIN_API_KEY=$token"
