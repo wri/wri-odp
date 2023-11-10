056842d39730d0b58df4266648a6419fdde5949d
MuhammadIsmailShahzad <ismailshahzadmirza@gmail.com>
2023-11-11 01:14:19 +0500

    Fix cypress script

--- ckan-backend-dev/ckan/scripts/cypress_setup.sh
+++ ckan-backend-dev/ckan/scripts/cypress_setup.sh
@@ -18,4 +18,4 @@ cp ../../../integration-tests/cypress.json.example ../../../integration-tests/cy
 # Replace the token placeholder with the generated token
 sed -i "s/CKAN_API_TOKEN/$token/g" ../../../integration-tests/cypress.json
 sed -i "s/CKAN_API_TOKEN/$token/g" ../../../e2e-tests/cypress.config.js
-sed -i "s/CKAN_API_TOKEN/$token/g" ../.env.frontend.example
+sed -i "s/CKAN_API_TOKEN/$token/g" ../../.env.frontend.example
