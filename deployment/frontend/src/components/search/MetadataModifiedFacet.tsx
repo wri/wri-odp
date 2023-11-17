957c4ad60441d4997605d81a860c7c17aca1f18d
Michael Polidori <contact@michaelpolidori.com>
2023-11-17 13:02:55 -0500

    Fix placeholder and type

--- deployment/frontend/src/components/search/MetadataModifiedFacet.tsx
+++ deployment/frontend/src/components/search/MetadataModifiedFacet.tsx
@@ -99,6 +99,7 @@ export default function MetadataModifiedFacet({
                                                 className="h-8 w-[8rem] rounded border-gray-300 text-gray-500 focus:ring-gray-500 px-3"
                                                 value={optionsState.metadata_modified_since}
                                                 onChange={e => handleDateChange('metadata_modified_since', e.target.value)}
+                                                placeholder='mm/dd/yyyy'
                                             />
                                         </div>
                                     </div>
@@ -118,6 +119,7 @@ export default function MetadataModifiedFacet({
                                                 className="h-8 w-[8rem] rounded border-gray-300 text-gray-500 focus:ring-gray-500 px-3"
                                                 value={optionsState.metadata_modified_before}
                                                 onChange={e => handleDateChange('metadata_modified_before', e.target.value)}
+                                                placeholder='mm/dd/yyyy'
                                             />
                                         </div>
                                     </div>
