5dfdd5bc66ee11a5e286d26395e58fffa1440705
Michael Polidori <contact@michaelpolidori.com>
2023-12-18 13:18:00 -0500

    Fix notification typo

--- deployment/frontend/src/components/dashboard/notification/NotificationCard.tsx
+++ deployment/frontend/src/components/dashboard/notification/NotificationCard.tsx
@@ -41,7 +41,7 @@ function Card({
             <div className="flex items-center">
                 <DefaultTooltip content="mark as read">
                     <input
-                        id="notificatoin"
+                        id="notification"
                         aria-describedby="notifications-checkbox"
                         name="notifications"
                         type="checkbox"
