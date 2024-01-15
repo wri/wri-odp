63f3a99f7c8a575f7f7fea0affed9b73ce8237cf
Michael Polidori <contact@michaelpolidori.com>
2024-01-15 09:25:45 -0500

    Run WRI unit tests first in src

--- ckan-backend-dev/ckan/scripts/run_unit_tests.sh
+++ ckan-backend-dev/ckan/scripts/run_unit_tests.sh
@@ -27,12 +27,26 @@ if [ -d "src_extensions/ckanext-wri" ]; then
 
 fi
 
+if [ -d "$ROOT_DIR/src/ckanext-wri" ]; then
+  cd $ROOT_DIR/src/ckanext-wri
+
+  pytest --ckan-ini=test.ini ckanext/wri/tests 2>&1 | tee -a "$ROOT_DIR/test_results.txt"
+  PYTEST_EXIT_CODE=${PIPESTATUS[0]}
+
+  if [ $PYTEST_EXIT_CODE -eq 0 ]; then
+    echo "ckanext-wri: Passed" >> "$ROOT_DIR/test_summary.txt"
+  else
+    echo "ckanext-wri: Failed" >> "$ROOT_DIR/test_summary.txt"
+  fi
+
+fi
+
 cd $ROOT_DIR/src
 
 for dir in ckanext-*; do
   if [ -d "$dir" ]; then
     # Skip ckanext-envvars
-    if [ "$dir" == "ckanext-envvars" ]; then
+    if [ "$dir" == "ckanext-envvars" || "$dir" == "ckanext-wri" ]; then
       continue
     fi
 
