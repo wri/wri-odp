c315aab95f360ced710bbc4d04dde4c23ea66a86
Luccas Mateus <luccasmmg@gmail.com>
2024-01-23 14:37:35 -0300

    Fix tests

--- ckan-backend-dev/ckan/scripts/init-extensions.sh
+++ ckan-backend-dev/ckan/scripts/init-extensions.sh
@@ -3,4 +3,4 @@
 docker exec ckan-wri sh -c "ckan -c production.ini issuesdb"
 docker exec ckan-wri sh -c "ckan -c production.ini notificationdb"
 docker exec ckan-wri sh -c "ckan -c production.ini pendingdatasetsdb"
-docker exec ckan-wri sh -c "ckan -c production.ini 'ckan.datapusher.api_token=$(ckan -c production.ini user token add ckan_admin datapusher | tail -n 1 | tr -d '\t')'"
+docker exec ckan-wri sh -c "ckan config-tool production.ini ckan.datapusher.api_token=$(ckan -c $CKAN_INI user token add ckan_admin datapusher | tail -n 1 | tr -d '\t')"
