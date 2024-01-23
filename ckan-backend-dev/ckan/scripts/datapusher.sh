9ae85db590d61cb5a4f3957762ac277aba29aa87
Luccas Mateus <luccasmmg@gmail.com>
2024-01-23 18:53:57 -0300

    Fix tests

--- ckan-backend-dev/ckan/scripts/datapusher.sh
+++ ckan-backend-dev/ckan/scripts/datapusher.sh
@@ -1,3 +1,5 @@
 echo "Changing Datapusher API Key"
+echo $(ckan -c production.ini user token add ckan_admin datapusher_test | tail -n 1 | tr -d '\t')
 ckan config-tool production.ini ckan.datapusher.api_token=$(ckan -c production.ini user token add ckan_admin datapusher | tail -n 1 | tr -d '\t')
+cat production.ini | grep api_token
 
