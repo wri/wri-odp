e31022a83abbd4d189a58548d36500b31b3d410c
steveoni <steohenoni2@gmail.com>
2024-03-07 16:05:18 +0100

    update getFacorite

--- deployment/frontend/src/pages/dashboard/datasets/index.tsx
+++ deployment/frontend/src/pages/dashboard/datasets/index.tsx
@@ -5,8 +5,50 @@ import DatasetList from '@/components/dashboard/datasets/DatasetList'
 import Footer from '@/components/_shared/Footer'
 import { NextSeo } from 'next-seo'
 import { env } from '@/env.mjs'
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
+import superjson from 'superjson'
+import { createServerSideHelpers } from '@trpc/react-query/server'
+import { appRouter } from '@/server/api/root'
+import { getServerAuthSession } from '../../../server/auth'
 
-export default function DatasetListPage() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const session = await getServerAuthSession(context)
+    const helpers = createServerSideHelpers({
+        router: appRouter,
+        ctx: { session },
+        transformer: superjson,
+    })
+
+    await helpers.notification.getAllNotifications.prefetch()
+    await helpers.user.getUserCapacity.prefetch()
+    await helpers.dataset.getAllDataset.prefetch({
+        search: '',
+        page: { start: 0, rows: 10 },
+        _isUserSearch: true,
+        fq: {
+            is_approved: 'true',
+            draft: 'false',
+        },
+    })
+   
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth/signin',
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        props: {
+            trpcState: helpers.dehydrate(),
+            session,
+        },
+    }
+}
+
+export default function DatasetListPage(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
         <>
             <NextSeo
