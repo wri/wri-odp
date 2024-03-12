765c74c5e6deafc5db3e0105f3bbb5fafb5db29c
Demenech <joaommdemenech@gmail.com>
2024-03-12 15:22:07 -0300

    fix; change owner of spatial_ref_sys

--- ckan-backend-dev/postgresql/docker-entrypoint-initdb.d/50_enable_postgis.sh
+++ ckan-backend-dev/postgresql/docker-entrypoint-initdb.d/50_enable_postgis.sh
@@ -7,6 +7,7 @@ echo "Initializing Postgis"
 echo "Loading PostGIS extensions into $CKAN_DB"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$CKAN_DB" <<-EOSQL
     CREATE EXTENSION postgis;
+    ALTER TABLE spatial_ref_sys OWNER TO $CKAN_DB_USER;
 EOSQL
 
 echo "Postgis initialized"
