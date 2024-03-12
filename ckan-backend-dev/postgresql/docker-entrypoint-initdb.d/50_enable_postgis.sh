5b3af9a83295978d88675b388da0a2776ef210d2
Demenech <joaommdemenech@gmail.com>
2024-03-12 13:42:27 -0300

    fix: update enable_postgis script

--- ckan-backend-dev/postgresql/docker-entrypoint-initdb.d/50_enable_postgis.sh
+++ ckan-backend-dev/postgresql/docker-entrypoint-initdb.d/50_enable_postgis.sh
@@ -4,17 +4,9 @@ set -e
 
 echo "Initializing Postgis"
 
-# Load PostGIS into both template_database and $POSTGRES_DB
-for DB in "$CKAN_DB"; do
-	echo "Loading PostGIS extensions into $DB"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-		CREATE EXTENSION IF NOT EXISTS postgis;
-		-- CREATE EXTENSION IF NOT EXISTS postgis_topology;
-		-- Reconnect to update pg_setting.resetval
-		-- See https://github.com/postgis/docker-postgis/issues/288
-		-- CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-		-- CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+echo "Loading PostGIS extensions into $CKAN_DB"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE EXTENSION postgis;
 EOSQL
-done
 
 echo "Postgis initialized"
