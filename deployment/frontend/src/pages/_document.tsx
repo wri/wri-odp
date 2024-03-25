8782bf5abf522ec0442852474a912f75844a280d
steveoni <steohenoni2@gmail.com>
2024-03-25 18:39:18 +0100

    remove fragment: does not render children in head tag

--- deployment/frontend/src/pages/_document.tsx
+++ deployment/frontend/src/pages/_document.tsx
@@ -14,7 +14,6 @@ export default class CustomDocument extends Document {
                    
                     {
                         env.NEXT_PUBLIC_DISABLE_HOTJAR === "disabled" ? "" :
-                            <>
                                  <Script
                         strategy="afterInteractive"
                         id="show-banner"
@@ -25,14 +24,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${env.NEXT_PUBLIC_GTM_ID}');`,
                         }}
-                    />
+                                />
+                    }
+                    {
+                        env.NEXT_PUBLIC_DISABLE_HOTJAR === "disabled" ? "" :
                                 <Script
                         strategy="afterInteractive"
                         id="hotjar"
                         dangerouslySetInnerHTML={{
                             __html: `(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3904211,hjsv:6};
+        h._hjSettings={hjid:${env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
@@ -41,7 +43,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 `,
                         }}
                     />
-                            </>
                             
                     }
                     
