739c06900fd88b83bc85309d8821a5ead3c71c93
steveoni <steohenoni2@gmail.com>
2024-03-25 18:42:56 +0100

    track resource title

--- deployment/frontend/src/components/datasets/sections/DataFiles.tsx
+++ deployment/frontend/src/components/datasets/sections/DataFiles.tsx
@@ -383,6 +383,7 @@ export function DataFiles({
             <DownloadModal
                 keys={datafilesToDownload.map((r) => r.key).filter(Boolean) as string[]}
                 dataset_id={dataset.id}
+                resource_name={datafilesToDownload.map((r) => r.title || r.name).join(",")}
                 open={open}
                 setOpen={setOpen}
             />
@@ -738,11 +739,13 @@ function DownloadModal({
     setOpen,
     dataset_id,
     keys,
+    resource_name
 }: {
     open: boolean
     setOpen: (open: boolean) => void
     dataset_id: string
     keys: string[]
+    resource_name: string
 }) {
     const formSchema = z.object({
         email: z.string().email(),
@@ -780,7 +783,7 @@ function DownloadModal({
                 {!isLoading && (
                     <form
                         id="download"
-                        data-resource={dataset_id}
+                        data-resource={resource_name}
                         onSubmit={handleSubmit(
                             async (data) => {
                                 downloadZipped.mutate(
