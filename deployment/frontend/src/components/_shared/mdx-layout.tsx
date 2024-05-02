d0706069bb74b67ae8e442b2ac00d15c2d4a327f
steveoni <steohenoni2@gmail.com>
2024-05-02 13:44:49 +0100

    adjust paragraph

--- deployment/frontend/src/components/_shared/mdx-layout.tsx
+++ deployment/frontend/src/components/_shared/mdx-layout.tsx
@@ -53,7 +53,7 @@ export default function MdxLayout({
                     </h1>
                 </div>
             </section>
-            <main className="px-8 mb-20 xxl:px-0  max-w-4xl mx-auto flex flex-col font-acumin mt-16 prose sm:prose-sm md:prose-base prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-md prose-h6:text-lg prose-p:mb-1 dark:prose-headings:text-white">
+            <main className="px-8 mb-20 xxl:px-0  max-w-4xl mx-auto flex flex-col font-acumin mt-16 prose sm:prose-sm md:prose-base prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-md prose-h6:text-lg prose-p:my-1 prose-li:my-1 dark:prose-headings:text-white">
                 {children}
             </main>
 
