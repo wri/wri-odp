81f0111e95d44919f8a5849738694dda990d60a5
Luccas Mateus <Luccasmmg@gmail.com>
2024-05-13 12:30:50 -0300

    Odp 244 (#447)
    * ODP-244 Start
    
    * Using GFW Custom Ortto form
    
    * Fix test
    
    * Fix tests

---
+++ deployment/frontend/src/pages/api/proxy-ortto.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import qs from 'query-string'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const body = JSON.parse(req.body)
+    const response = await fetch('https://ortto.wri.org/custom-forms/gfw/', {
+        method: 'POST',
+        body: qs.stringify(body),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+        },
+    })
+    const json = await response.json()
+
+    return res.json(json)
+}
