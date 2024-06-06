555da213aee22105785a14625015ebcffa367b9c
Luccas Mateus <Luccasmmg@gmail.com>
2024-06-06 20:19:41 -0300

    Fix 404 (#481)
    * Fix internal server error
    
    * Fix 404
    
    * Remove console.log
    
    * Fix workflow

--- deployment/frontend/src/pages/datasets/[datasetName]/index.tsx
+++ deployment/frontend/src/pages/datasets/[datasetName]/index.tsx
@@ -202,7 +202,6 @@ export async function getServerSideProps(
             dataset.resources = []
         }
         const NEXTURL = env.NEXTAUTH_URL
-        console.timeEnd('datasetPage')
         return {
             props: {
                 NEXTURL,
