812a76aad5182960aebb4eb8042e33aed502ede9
steveoni <steohenoni2@gmail.com>
2024-07-22 16:09:25 +0100

    remove create and edit datset api doc

--- deployment/frontend/src/components/datasets/sections/API.tsx
+++ deployment/frontend/src/components/datasets/sections/API.tsx
@@ -119,63 +119,6 @@ const QueryInstructions = () => {
                 description="Get this dataset's metadata"
                 url={ckanDatasetGetUrl}
             />
-            <QueryEndpoint
-                description="Create a new Dataset"
-                method="POST"
-                url={`${publicCkanUrl}/api/3/action/package_create`}
-                headers={{
-                    Authorization: '<API_TOKEN>',
-                }}
-                body={`{
-    "name": ${dataset.name},
-    "title": ${dataset.title},
-    "visibility_type": "public",
-    "short_description": ${dataset.short_description},
-    "technical_notes": ${dataset.technical_notes},
-    "maintainer": ${dataset.maintainer},
-    "maintainer_email": ${dataset.maintainer_email},
-    "author": ${dataset.author},
-    "resources": [
-        {
-            "format": "CSV",
-            "name": "Test.csv",
-            "description": "Test description",
-            "type": "link",
-            "url": "https://test.com/a_link_to_csv_file.csv",
-
-        }
-    ]
-}`}
-            />
-            <QueryEndpoint
-                description="Edit a Dataset"
-                method="POST"
-                url={`${publicCkanUrl}/api/3/action/package_patch`}
-                headers={{
-                    Authorization: '<API_TOKEN>',
-                }}
-                body={`{
-    "id": "${dataset.id}",
-    "name": "${dataset.name}",
-    "title": "${dataset.title}+ - Edited",
-    "visibility_type": "public",
-    "short_description": "${dataset.short_description}",
-    "technical_notes": "${dataset.technical_notes}",
-    "maintainer": "${dataset.maintainer}",
-    "maintainer_email": "${dataset.maintainer_email}",
-    "author": "${dataset.author}",
-    "resources": [
-        {
-            "format": "CSV",
-            "name": "Test_Edited.csv",
-            "description": "Test description Edited",
-            "type": "link",
-            "url": "https://test.com/a_link_to_csv_file_edited.csv",
-
-        }
-    ]
-}`}
-            />
 
             <CkanApiMoreInfo />
 
@@ -289,70 +232,7 @@ const SnippetInstructions = ({
                 snippet={ckanPackageShowSnippet}
                 language={language}
             />
-            <SnippetEndpoint
-                description="Create a new Dataset"
-                snippet={getSnippetFn(
-                    `${ckanBaseUrl}/package_create`,
-                    'POST',
-                    JSON.stringify(
-                        {
-                            name: dataset.name,
-                            title: dataset.title,
-                            visibility_type: 'public',
-                            short_description: dataset.short_description,
-                            technical_notes: dataset.technical_notes,
-                            maintainer: dataset.maintainer,
-                            maintainer_email: dataset.maintainer_email,
-                            author: dataset.author,
-                            resources: [
-                                {
-                                    format: 'CSV',
-                                    name: 'Test.csv',
-                                    description: 'Test description',
-                                    type: 'link',
-                                    url: 'https://test.com/a_link_to_csv_file.csv',
-                                },
-                            ],
-                        },
-                        null,
-                        4
-                    ).replace(/\n/g, `\n${' '.repeat(8)}`)
-                )}
-                language={language}
-            />
 
-            <SnippetEndpoint
-                description="Edit a Dataset"
-                snippet={getSnippetFn(
-                    `${ckanBaseUrl}/package_patch`,
-                    'POST',
-                    JSON.stringify(
-                        {
-                            name: dataset.name,
-                            title: dataset.title,
-                            visibility_type: 'public',
-                            short_description:
-                                dataset.short_description + ' - Edited',
-                            technical_notes: dataset.technical_notes,
-                            maintainer: dataset.maintainer,
-                            maintainer_email: dataset.maintainer_email,
-                            author: dataset.author,
-                            resources: [
-                                {
-                                    format: 'CSV',
-                                    name: 'Test_Edited.csv',
-                                    description: 'Test description -Edited',
-                                    type: 'link',
-                                    url: 'https://test.com/a_link_to_csv_file_edited.csv',
-                                },
-                            ],
-                        },
-                        null,
-                        4
-                    ).replace(/\n/g, `\n${' '.repeat(8)}`)
-                )}
-                language={language}
-            />
             <CkanApiMoreInfo />
 
             {dataset.rw_id && (
