2d5998abb254025b58448d36cb934d8a01b3de6a
Michael Polidori <contact@michaelpolidori.com>
2024-09-19 10:47:02 -0400

    Disable hotjar on consent denial; Auto-reload page on consent save

--- deployment/frontend/src/pages/_document.tsx
+++ deployment/frontend/src/pages/_document.tsx
@@ -22,6 +22,17 @@ export default class CustomDocument extends Document {
                         'wait_for_update': 500
                       });
                       gtag("set", "ads_data_redaction", true);
+                      // Reloads page after Osano consent is saved
+                      // Needed so that the Google Tag Manager and Hotjar checks can run again
+                      const osanoCheckInterval = setInterval(function() {
+                          const saveButton = document.querySelector('.osano-cm-save');
+                          if (saveButton) {
+                              saveButton.addEventListener('click', function() {
+                                  window.location.reload();
+                              });
+                              clearInterval(osanoCheckInterval);
+                          }
+                      }, 500);
                       `
                     }} />
                     <script src={ env.OSANO_URL }></script>
@@ -49,15 +60,29 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         strategy="afterInteractive"
                         id="hotjar"
                         dangerouslySetInnerHTML={{
-                            __html: `(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:${env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-`,
+                            __html: `
+                                (function(){
+                                    function loadHotjar() {
+                                        // Disables Hotjar if user has denied analytics consent
+                                        var consent = Osano.cm.getConsent();
+                                        if (consent.ANALYTICS !== 'DENY') {
+                                            (function(h,o,t,j,a,r){
+                                                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                                                h._hjSettings={hjid:${env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
+                                                a=o.getElementsByTagName('head')[0];
+                                                r=o.createElement('script');r.async=1;
+                                                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                                                a.appendChild(r);
+                                            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                                        }
+                                    }
+                                    if (document.readyState === 'complete') {
+                                        loadHotjar();
+                                    } else {
+                                        window.addEventListener('load', loadHotjar);
+                                    }
+                                })();
+                                `,
                         }}
                     />
                     }
