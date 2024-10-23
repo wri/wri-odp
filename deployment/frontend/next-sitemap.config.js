e24f3b56b57b85ed7673122c81d07940fac7d7d1
steveoni <steohenoni2@gmail.com>
2024-10-23 12:48:03 +0100

    [sitemap][x]: remove diallow all from stiemap

--- deployment/frontend/next-sitemap.config.js
+++ deployment/frontend/next-sitemap.config.js
@@ -8,23 +8,16 @@ module.exports = {
       additionalSitemaps: [
           `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/server-sitemap.xml`,
       ],
-      policies:
-          process.env.NEXT_PUBLIC_DEPLOYMENT_TYPE === 'production'
-              ? [
-                    {
-                        userAgent: '*',
-                        disallow: ['/dashboard/*'],
-                    },
-                    {
-                        userAgent: '*',
-                        allow: '/',
-                    },
-                ]
-              : [
-                    {
-                        userAgent: '*',
-                        disallow: ['/*'],
-                    },
-                ],
+      policies:[
+            {
+                userAgent: '*',
+                disallow: ['/dashboard/*'],
+            },
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+        ]
+              
   },
 }
