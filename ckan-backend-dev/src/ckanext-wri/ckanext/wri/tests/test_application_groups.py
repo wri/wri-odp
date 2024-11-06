059270395fa74e044a3c693cfcb6c9d0e58cc3f1
Michael Polidori <contact@michaelpolidori.com>
2024-11-06 16:23:31 -0500

    Uncomment tests

--- ckan-backend-dev/src/ckanext-wri/ckanext/wri/tests/test_application_groups.py
+++ ckan-backend-dev/src/ckanext-wri/ckanext/wri/tests/test_application_groups.py
@@ -7,115 +7,115 @@ import unittest.mock as mock
 from ckan.logic import ValidationError
 
 
-# @mock.patch("ckan.plugins.toolkit.mail_user")
-# @pytest.mark.usefixtures("with_plugins", "test_request_context")
-# def test_application_group_create_all_fields(mail_user):
-#    userobj = factories.Sysadmin()
-#    session = model.Session
-#    context = {
-#        "model": model, "session": session,
-#        "user": userobj["name"], "ignore_auth": True,
-#        "user_obj": userobj
-#    }
-#    context["auth_user_obj"] = model.User.get(context["user"])
-#
-#    application_group = {
-#        "name": "test-application-group-schema",
-#        "title": "Test Application Group Schema",
-#        "description": "A description of the application group",
-#        "homepage_url": "http://example.com",
-#        "help_url": "http://example.com/help",
-#        "contact_url": "http://example.com/contact",
-#        "image_url": "http://example.com/image",
-#        "type": "application",
-#    }
-#
-#    try:
-#        get_action("group_purge")(
-#            context={"ignore_auth": True},
-#            data_dict={"id": application_group["name"]}
-#        )
-#    except Exception:
-#        pass
-#
-#    application_group = get_action("group_create")(context, application_group)
-#
-#    assert application_group["name"] == "test-application-group-schema"
-#    assert application_group["title"] == "Test Application Group Schema"
-#    assert application_group["description"] == "A description of the application group"
-#    assert application_group["homepage_url"] == "http://example.com"
-#    assert application_group["help_url"] == "http://example.com/help"
-#    assert application_group["contact_url"] == "http://example.com/contact"
-#    assert application_group["image_url"] == "http://example.com/image"
-#    assert application_group["type"] == "application"
-#
-#    get_action("group_purge")(
-#        context={"ignore_auth": True},
-#        data_dict={"id": application_group["name"]}
-#    )
-#
-#
-# @mock.patch("ckan.plugins.toolkit.mail_user")
-# @pytest.mark.usefixtures("with_plugins", "test_request_context")
-# def test_application_group_update_all_fields(mail_user):
-#    userobj = factories.Sysadmin()
-#    session = model.Session
-#    context = {
-#        "model": model, "session": session,
-#        "user": userobj["name"], "ignore_auth": True,
-#        "user_obj": userobj
-#    }
-#    context["auth_user_obj"] = model.User.get(context["user"])
-#
-#    application_group = {
-#        "name": "test-application-group-schema",
-#        "title": "Test Application Group Schema",
-#        "description": "A description of the application group",
-#        "homepage_url": "http://example.com",
-#        "help_url": "http://example.com/help",
-#        "contact_url": "http://example.com/contact",
-#        "image_url": "http://example.com/image",
-#        "type": "application",
-#    }
-#
-#    try:
-#        get_action("group_purge")(
-#            context={"ignore_auth": True},
-#            data_dict={"id": application_group["name"]}
-#        )
-#    except Exception:
-#        pass
-#
-#    application_group = get_action("group_create")(context, application_group)
-#
-#    application_group = get_action("group_update")(
-#        context, {
-#            "id": application_group["id"],
-#            "name": application_group["name"],
-#            "title": "New Title",
-#            "description": "New Description",
-#            "homepage_url": "http://new.example.com",
-#            "help_url": "http://new.example.com/help",
-#            "contact_url": "http://new.example.com/contact",
-#            "image_url": "http://new.example.com/image",
-#        }
-#    )
-#
-#    application_group = get_action("group_show")(context, {"id": application_group["id"]})
-#
-#    assert application_group["name"] == "test-application-group-schema"
-#    assert application_group["title"] == "New Title"
-#    assert application_group["description"] == "New Description"
-#    assert application_group["homepage_url"] == "http://new.example.com"
-#    assert application_group["help_url"] == "http://new.example.com/help"
-#    assert application_group["contact_url"] == "http://new.example.com/contact"
-#    assert application_group["image_url"] == "http://new.example.com/image"
-#    assert application_group["type"] == "application"
-#
-#    get_action("group_purge")(
-#        context={"ignore_auth": True},
-#        data_dict={"id": application_group["name"]}
-#    )
+@mock.patch("ckan.plugins.toolkit.mail_user")
+@pytest.mark.usefixtures("with_plugins", "test_request_context")
+def test_application_group_create_all_fields(mail_user):
+   userobj = factories.Sysadmin()
+   session = model.Session
+   context = {
+       "model": model, "session": session,
+       "user": userobj["name"], "ignore_auth": True,
+       "user_obj": userobj
+   }
+   context["auth_user_obj"] = model.User.get(context["user"])
+
+   application_group = {
+       "name": "test-application-group-schema",
+       "title": "Test Application Group Schema",
+       "description": "A description of the application group",
+       "homepage_url": "http://example.com",
+       "help_url": "http://example.com/help",
+       "contact_url": "http://example.com/contact",
+       "image_url": "http://example.com/image",
+       "type": "application",
+   }
+
+   try:
+       get_action("group_purge")(
+           context={"ignore_auth": True},
+           data_dict={"id": application_group["name"]}
+       )
+   except Exception:
+       pass
+
+   application_group = get_action("group_create")(context, application_group)
+
+   assert application_group["name"] == "test-application-group-schema"
+   assert application_group["title"] == "Test Application Group Schema"
+   assert application_group["description"] == "A description of the application group"
+   assert application_group["homepage_url"] == "http://example.com"
+   assert application_group["help_url"] == "http://example.com/help"
+   assert application_group["contact_url"] == "http://example.com/contact"
+   assert application_group["image_url"] == "http://example.com/image"
+   assert application_group["type"] == "application"
+
+   get_action("group_purge")(
+       context={"ignore_auth": True},
+       data_dict={"id": application_group["name"]}
+   )
+
+
+@mock.patch("ckan.plugins.toolkit.mail_user")
+@pytest.mark.usefixtures("with_plugins", "test_request_context")
+def test_application_group_update_all_fields(mail_user):
+   userobj = factories.Sysadmin()
+   session = model.Session
+   context = {
+       "model": model, "session": session,
+       "user": userobj["name"], "ignore_auth": True,
+       "user_obj": userobj
+   }
+   context["auth_user_obj"] = model.User.get(context["user"])
+
+   application_group = {
+       "name": "test-application-group-schema",
+       "title": "Test Application Group Schema",
+       "description": "A description of the application group",
+       "homepage_url": "http://example.com",
+       "help_url": "http://example.com/help",
+       "contact_url": "http://example.com/contact",
+       "image_url": "http://example.com/image",
+       "type": "application",
+   }
+
+   try:
+       get_action("group_purge")(
+           context={"ignore_auth": True},
+           data_dict={"id": application_group["name"]}
+       )
+   except Exception:
+       pass
+
+   application_group = get_action("group_create")(context, application_group)
+
+   application_group = get_action("group_update")(
+       context, {
+           "id": application_group["id"],
+           "name": application_group["name"],
+           "title": "New Title",
+           "description": "New Description",
+           "homepage_url": "http://new.example.com",
+           "help_url": "http://new.example.com/help",
+           "contact_url": "http://new.example.com/contact",
+           "image_url": "http://new.example.com/image",
+       }
+   )
+
+   application_group = get_action("group_show")(context, {"id": application_group["id"]})
+
+   assert application_group["name"] == "test-application-group-schema"
+   assert application_group["title"] == "New Title"
+   assert application_group["description"] == "New Description"
+   assert application_group["homepage_url"] == "http://new.example.com"
+   assert application_group["help_url"] == "http://new.example.com/help"
+   assert application_group["contact_url"] == "http://new.example.com/contact"
+   assert application_group["image_url"] == "http://new.example.com/image"
+   assert application_group["type"] == "application"
+
+   get_action("group_purge")(
+       context={"ignore_auth": True},
+       data_dict={"id": application_group["name"]}
+   )
 
 
 @mock.patch("ckan.plugins.toolkit.mail_user")
