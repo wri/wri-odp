3987b1ed4a1762c901f874517eb23cf8fe5f5660
Luccas Mateus <Luccasmmg@gmail.com>
2024-11-19 08:39:01 -0300

    Fix vulnerabilities (#587)

--- deployment/frontend/src/components/vizzuality/layer-manager/packages/core/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './layer-manager';
-export { default as LayerModel } from './layer-model';
--- deployment/frontend/src/components/vizzuality/layer-manager/packages/utils/src/omit.ts
+++
@@ -1,6 +0,0 @@
-export const omit = (key: string, obj: any): any => {
-  const { [key]: omitted, ...rest } = obj;
-  return rest;
-};
-
-export default omit;
