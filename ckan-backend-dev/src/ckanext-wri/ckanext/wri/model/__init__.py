e8ea7ae726a62da4827de6a3b0bd513c98237618
Michael Polidori <contact@michaelpolidori.com>
2025-01-27 14:32:34 -0500

    Initial upgrade to CKAN 2.11

--- ckan-backend-dev/src/ckanext-wri/ckanext/wri/model/__init__.py
+++ ckan-backend-dev/src/ckanext-wri/ckanext/wri/model/__init__.py
@@ -2,6 +2,7 @@ from .notification import Notification, notification
 from .pending_datasets import pending_datasets
 from .resource_location import resource_location
 from .download_event import download_event
+from ckan.model import ensure_engine
 import logging
 
 log = logging.getLogger(__name__)
@@ -13,8 +14,9 @@ def setup():
     """
     Create Notifications Table in the database.
     """
-    if not notification.exists():
-        notification.create(checkfirst=True)
+    engine = ensure_engine()
+    if not notification.exists(engine):
+        notification.create(engine, checkfirst=True)
         log.info("Tables created for notifications")
     else:
         log.info("Notifications Table already exists")
@@ -24,23 +26,26 @@ def setup_pending_datasets():
     """
     Create Pending Datasets Table in the database.
     """
-    if not pending_datasets.exists():
-        pending_datasets.create(checkfirst=True)
+    engine = ensure_engine()
+    if not pending_datasets.exists(engine):
+        pending_datasets.create(engine, checkfirst=True)
         log.info("Tables created for pending datasets")
     else:
         log.info("Pending Datasets Table already exists")
 
 
 def setup_resource_location():
-    if not resource_location.exists():
-        resource_location.create(checkfirst=True)
+    engine = ensure_engine()
+    if not resource_location.exists(engine):
+        resource_location.create(engine, checkfirst=True)
         log.info("Resource location table created")
     else:
         log.info("Resource location table already exists")
 
 def setup_download_event():
-    if not download_event.exists():
-        download_event.create(checkfirst=True)
+    engine = ensure_engine()
+    if not download_event.exists(engine):
+        download_event.create(engine, checkfirst=True)
         log.info("Download event table created")
     else:
         log.info("Download event table already exists")
