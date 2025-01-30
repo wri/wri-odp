d0e0366501af9b230bce4e590f25e14283127440
steveoni <steohenoni2@gmail.com>
2025-01-30 19:12:11 +0100

    update filter scrolltop

--- deployment/frontend/src/pages/search.tsx
+++ deployment/frontend/src/pages/search.tsx
@@ -249,9 +249,7 @@ export default function SearchPage(
                 shallow: true,
             }
         )
-        if (window) {
-            window.scrollTo({ top: 0, behavior: 'smooth' })
-        }
+        
     }, [filters, query.page, query.sortBy])
     const links = [{ label: 'Search', url: '/search', current: true }]
 
