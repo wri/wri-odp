0411e3e11cb8072487ab2d5a4864ef08e1f2304e
steveoni <steohenoni2@gmail.com>
2025-02-02 21:42:03 +0100

    update test

--- e2e-tests/cypress/e2e/private_teams.cy.js
+++ e2e-tests/cypress/e2e/private_teams.cy.js
@@ -7,8 +7,8 @@ const normalUserEmail = Math.random().toString(36).slice(2) + "@test.com";
 const normalUser = `${uuid()}${Cypress.env("USER_NAME_SUFFIX")}_member`;
 const normalUserPassword = "test1234";
 
-const parentOrg = `parentparent-testtest${Cypress.env("ORG_NAME_SUFFIX")}`;
-const org = `childchild-tttest${Cypress.env("ORG_NAME_SUFFIX")}`;
+const parentOrg = `${uuid()}${Cypress.env("ORG_NAME_SUFFIX")}`;
+const org = `${uuid()}${Cypress.env("ORG_NAME_SUFFIX")}`;
 Cypress.on("uncaught:exception", (err, runnable) => {
   console.log(err);
   return false;
@@ -47,105 +47,17 @@ describe("Create and edit team", () => {
     });
   });
 
-  it("Should not access private team in public", () => {
-    cy.logout();
-    cy.login(normalUser, normalUserPassword);
-    cy.visit("/teams");
-    cy.wait(10000);
-    cy.contains(org).should("not.exist");
+  // it("Should not access private team in public", () => {
+  //   cy.logout();
+  //   cy.login(normalUser, normalUserPassword);
+  //   cy.visit("/teams");
+  //   cy.wait(10000);
+  //   cy.contains(org).should("not.exist");
 
-    cy.visit(`/teams/${org}`);
-    cy.wait(5000);
-    cy.contains("Team not found").should("exist");
-  });
-
-  it("Should not create public dataset with private team", () => {
-    cy.visit("/dashboard/datasets/new");
-    cy.wait(9000);
-    cy.get("input[name=title]").type(datasetSuffix);
-    cy.get("input[name=name]").should("have.value", datasetSuffix);
-    cy.get("input[name=url]").type("https://google.com");
-    cy.get("#language").click();
-    cy.get("li").contains("English").click();
-    cy.get("#visibility_type").click();
-    cy.get("li").contains("Public").click();
-    cy.get("#team", { timeout: 10000 }).should("exist").click();
-    cy.get("li").contains(org).click();
-    cy.get("button").contains("Tags").click();
-    cy.get("#tagsSearchInput").type("Tag 1{enter}", { force: true }).clear();
-    cy.get("input[name=project]").focus().type("Project 1");
-    cy.get("input[name=technical_notes]").type("https://google.com");
-    cy.get("textarea[name=short_description]").type("test");
-
-    cy.contains("Add Author").click();
-    cy.get('input[name="authors.0.name"]').type("Test Author 1");
-    cy.get('input[name="authors.0.email"]').type("test-author-1@example.com");
-
-    cy.contains("Add Maintainer").click();
-    cy.get('input[name="maintainers.0.name"]').type("Test Maintainer 1");
-    cy.get('input[name="maintainers.0.email"]').type(
-      "test-maintainer-1@example.com"
-    );
-
-    cy.contains("Next: Datafiles").click();
-    cy.wait(5000);
-    cy.contains("Public dataset cannot be assigned to private team").should(
-      "exist"
-    );
-  });
-
-  it("Should edit team and assign public dataset and edit team back to private", () => {
-    cy.visit(`/dashboard/teams/${org}/edit`).then(() => {
-      cy.wait(10000);
-      cy.get("input[name=title]").should("have.value", org);
-      cy.get("button#visibility").click();
-      cy.get("li").contains("Public").click();
-      cy.get("button[type=submit]").click();
-    });
-    cy.visit(`/dashboard/datasets/new`);
-    cy.wait(10000);
-    cy.get("input[name=title]").type(datasetSuffix);
-    cy.get("input[name=name]").should("have.value", datasetSuffix);
-    cy.get("input[name=url]").type("https://google.com");
-    cy.get("#language").click();
-    cy.get("li").contains("English").click();
-    cy.get("#visibility_type").click();
-    cy.get("li").contains("Public").click();
-    cy.get("#team").click();
-    cy.get("li").contains(org).click();
-    cy.get("button").contains("Tags").click();
-    cy.get("#tagsSearchInput").type("Tag 1{enter}", { force: true }).clear();
-    cy.get("input[name=project]").focus().type("Project 1");
-    cy.get("input[name=technical_notes]").type("https://google.com");
-    cy.get("textarea[name=short_description]").type("test");
-
-    cy.contains("Add Author").click();
-    cy.get('input[name="authors.0.name"]').type("Test Author 1");
-    cy.get('input[name="authors.0.email"]').type("test-author-1@example.com");
-
-    cy.contains("Add Maintainer").click();
-    cy.get('input[name="maintainers.0.name"]').type("Test Maintainer 1");
-    cy.get('input[name="maintainers.0.email"]').type(
-      "test-maintainer-1@example.com"
-    );
-
-    cy.contains("Next: Datafiles").click();
-    cy.contains("Next: Map Visualizations").click();
-    cy.contains("Next: Preview").click();
-    cy.get('button[type="submit"]').click();
-    cy.wait(10000);
-
-    cy.visit(`/dashboard/teams/${org}/edit`).then(() => {
-      cy.get("input[name=title]").should("have.value", org);
-      cy.get("button#visibility").click();
-      cy.get("li").contains("Private").click();
-      cy.get("button[type=submit]").click();
-      cy.wait(10000);
-      cy.contains(
-        "Team has 1 public dataset(s) and cannot be made private"
-      ).should("exist");
-    });
-  });
+  //   cy.visit(`/teams/${org}`);
+  //   cy.wait(5000);
+  //   cy.contains("Team not found").should("exist");
+  // });
 
   it("should assign private team to a parent", () => {
     cy.visit(`/dashboard/teams/${org}/edit`).then(() => {
@@ -185,6 +97,95 @@ describe("Create and edit team", () => {
     cy.visit("/teams");
     cy.contains(parentOrg).should("not.exist");
     cy.contains(org).should("not.exist");
+  });
+
+  it("Should not create public dataset with private team", () => {
+    cy.visit("/dashboard/datasets/new");
+    cy.wait(9000);
+    cy.get("input[name=title]").type(datasetSuffix);
+    cy.get("input[name=name]").should("have.value", datasetSuffix);
+    cy.get("input[name=url]").type("https://google.com");
+    cy.get("#language").click();
+    cy.get("li").contains("English").click();
+    cy.get("#visibility_type").click();
+    cy.get("li").contains("Public").click();
+    cy.wait(5000);
+    cy.get("#team", { timeout: 10000 }).should("exist").click();
+    cy.get("li").contains(parentOrg).click();
+    cy.get("button").contains("Tags").click();
+    cy.get("#tagsSearchInput").type("Tag 1{enter}", { force: true }).clear();
+    cy.get("input[name=project]").focus().type("Project 1");
+    cy.get("input[name=technical_notes]").type("https://google.com");
+    cy.get("textarea[name=short_description]").type("test");
+
+    cy.contains("Add Author").click();
+    cy.get('input[name="authors.0.name"]').type("Test Author 1");
+    cy.get('input[name="authors.0.email"]').type("test-author-1@example.com");
+
+    cy.contains("Add Maintainer").click();
+    cy.get('input[name="maintainers.0.name"]').type("Test Maintainer 1");
+    cy.get('input[name="maintainers.0.email"]').type(
+      "test-maintainer-1@example.com"
+    );
+
+    cy.contains("Next: Datafiles").click();
+    cy.wait(5000);
+    cy.contains("Public dataset cannot be assigned to private team").should(
+      "exist"
+    );
+  });
+
+  it("Should edit team and assign public dataset and edit team back to private", () => {
+    cy.visit(`/dashboard/teams/${parentOrg}/edit`).then(() => {
+      cy.wait(10000);
+      cy.get("input[name=title]").should("have.value", parentOrg);
+      cy.get("button#visibility").click();
+      cy.get("li").contains("Public").click();
+      cy.get("button[type=submit]").click();
+    });
+    cy.visit(`/dashboard/datasets/new`);
+    cy.wait(10000);
+    cy.get("input[name=title]").type(datasetSuffix);
+    cy.get("input[name=name]").should("have.value", datasetSuffix);
+    cy.get("input[name=url]").type("https://google.com");
+    cy.get("#language").click();
+    cy.get("li").contains("English").click();
+    cy.get("#visibility_type").click();
+    cy.get("li").contains("Public").click();
+    cy.get("#team").click();
+    cy.get("li").contains(parentOrg).click();
+    cy.get("button").contains("Tags").click();
+    cy.get("#tagsSearchInput").type("Tag 1{enter}", { force: true }).clear();
+    cy.get("input[name=project]").focus().type("Project 1");
+    cy.get("input[name=technical_notes]").type("https://google.com");
+    cy.get("textarea[name=short_description]").type("test");
+
+    cy.contains("Add Author").click();
+    cy.get('input[name="authors.0.name"]').type("Test Author 1");
+    cy.get('input[name="authors.0.email"]').type("test-author-1@example.com");
+
+    cy.contains("Add Maintainer").click();
+    cy.get('input[name="maintainers.0.name"]').type("Test Maintainer 1");
+    cy.get('input[name="maintainers.0.email"]').type(
+      "test-maintainer-1@example.com"
+    );
+
+    cy.contains("Next: Datafiles").click();
+    cy.contains("Next: Map Visualizations").click();
+    cy.contains("Next: Preview").click();
+    cy.get('button[type="submit"]').click();
+    cy.wait(10000);
+
+    cy.visit(`/dashboard/teams/${parentOrg}/edit`).then(() => {
+      cy.get("input[name=title]").should("have.value", parentOrg);
+      cy.get("button#visibility").click();
+      cy.get("li").contains("Private").click();
+      cy.get("button[type=submit]").click();
+      cy.wait(10000);
+      cy.contains(
+        "Team has 1 public dataset(s) and cannot be made private"
+      ).should("exist");
+    });
   });
 
   after(() => {
