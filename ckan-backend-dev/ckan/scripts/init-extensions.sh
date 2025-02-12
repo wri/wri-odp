1ab0e363698fc534f5c230590adac1859500e534
Luccas Mateus <luccasmmg@gmail.com>
2025-02-12 09:18:17 -0300

    Rm ckanext-issues

--- ckan-backend-dev/ckan/scripts/init-extensions.sh
+++ ckan-backend-dev/ckan/scripts/init-extensions.sh
@@ -1,6 +1,6 @@
 
 # Initialize Issues and Notification DB
-docker exec ckan-wri sh -c "ckan -c production.ini issuesdb"
+#docker exec ckan-wri sh -c "ckan -c production.ini issuesdb"
 docker exec ckan-wri sh -c "ckan -c production.ini downloadeventdb"
 docker exec ckan-wri sh -c "ckan -c production.ini notificationdb"
 docker exec ckan-wri sh -c "ckan -c production.ini pendingdatasetsdb"
