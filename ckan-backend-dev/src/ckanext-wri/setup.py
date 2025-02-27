94166a0205e17bda9334352448970a8c39e3abc8
Luccas Mateus <luccasmmg@gmail.com>
2025-02-27 20:00:17 -0300

    Update setup.py

--- ckan-backend-dev/src/ckanext-wri/setup.py
+++ ckan-backend-dev/src/ckanext-wri/setup.py
@@ -1,7 +1,71 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
+    name='''ckanext-wri''',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='1.2.2',
+
+    description='''CKAN Extension for WRI''',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/wri/wri-odp',
+
+    # Author details
+    author='''WRI''',
+
+    keywords='''CKAN''',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    namespace_packages=['ckanext'],
+
+    install_requires=[
+      # CKAN extensions should not list dependencies here, but in a separate
+      # ``requirements.txt`` file.
+      #
+      # http://docs.ckan.org/en/latest/extensions/best-practices.html#add-third-party-libraries-to-requirements-txt
+    ],
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    include_package_data=True,
+    package_data={
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points='''
+        [ckan.plugins]
+        wri=ckanext.wri.plugin:WriPlugin
+        wri_api_tracking=ckanext.wri.plugin:WriApiTracking
+        wri_harvester=ckanext.wri.harvesters.wri_harvester:CKANHarvesterWRI
+
+        [babel.extractors]
+        ckan = ckan.lib.extract:extract_ckan
+    ''',
+
     # If you are changing from the default layout of your extension, you may
     # have to change the message extractors, you can read more about babel
     # message extraction at
