e6e647ab108ac71f53398eea3723479ab0e7579f
Luccas Mateus <luccasmmg@gmail.com>
2025-03-17 18:06:32 -0300

    Rm global from DatafileLocation

--- deployment/frontend/src/components/dashboard/datasets/admin/datafiles/DatafileLocation.tsx
+++ deployment/frontend/src/components/dashboard/datasets/admin/datafiles/DatafileLocation.tsx
@@ -123,7 +123,6 @@ export function DatafileLocation({
                     )
                         .with('geom', () => 0)
                         .with('address', () => 1)
-                        .with('global', () => 2)
                         .otherwise(() => undefined)}
                 >
                     <Tab.List
@@ -200,42 +199,6 @@ export function DatafileLocation({
                                 </span>
                             )}
                         </Tab>
-                        <Tab
-                            id="tabLink"
-                            onClick={() => {
-                                setValue(
-                                    `resources.${index}.spatial_type`,
-                                    'global'
-                                )
-                                setValue(
-                                    `resources.${index}.spatial_geom`,
-                                    undefined
-                                )
-                                setValue(
-                                    `resources.${index}.spatial_address`,
-                                    'Global'
-                                )
-                            }}
-                        >
-                            {({ selected }) => (
-                                <span
-                                    className={classNames(
-                                        'group flex aspect-square w-full flex-col items-center justify-center rounded-sm border-b-2 border-amber-400 bg-neutral-100 shadow transition hover:bg-amber-400 md:gap-y-2',
-                                        selected ? 'bg-amber-400' : ''
-                                    )}
-                                >
-                                    <GlobeEuropeAfricaIcon className="h-5 w-5 text-blue-800 sm:h-9 sm:w-9" />
-                                    <div
-                                        className={classNames(
-                                            'font-acumin text-xs font-normal text-black group-hover:font-bold sm:text-sm',
-                                            selected ? 'font-bold' : ''
-                                        )}
-                                    >
-                                        Global Datafile
-                                    </div>
-                                </span>
-                            )}
-                        </Tab>
                     </Tab.List>
                     <Tab.Panels as="div" className="mt-2">
                         <Tab.Panel>
@@ -308,13 +271,14 @@ export function DatafileLocation({
                                             undefined
                                         )
                                     }}
-                                    initialValue={watch(
-                                        `resources.${index}.spatial_address`
-                                    ) ?? undefined}
+                                    initialValue={
+                                        watch(
+                                            `resources.${index}.spatial_address`
+                                        ) ?? undefined
+                                    }
                                 />
                             </Map>
                         </Tab.Panel>
-                        <Tab.Panel></Tab.Panel>
                     </Tab.Panels>
                 </Tab.Group>
             </Disclosure.Panel>
