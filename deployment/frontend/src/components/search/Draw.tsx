4db49820682adf47c39d7ef25d2d7c8918a354d1
Luccas Mateus <luccasmmg@gmail.com>
2025-03-19 11:20:25 -0300

    Fix typo

--- deployment/frontend/src/components/search/Draw.tsx
+++ deployment/frontend/src/components/search/Draw.tsx
@@ -51,7 +51,7 @@ export default function DrawControl(props: DrawControlProps) {
             )
             if (button) {
                 button.title =
-                    'Area Select Tool, use this to create a reactangle that can be used as a bounding box for your search'
+                    'Area Select Tool, use this to create a rectangle that can be used as a bounding box for your search'
                 button.innerHTML = `
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-dashed"><path d="M5 3a2 2 0 0 0-2 2"/><path d="M19 3a2 2 0 0 1 2 2"/><path d="M21 19a2 2 0 0 1-2 2"/><path d="M5 21a2 2 0 0 1-2-2"/><path d="M9 3h1"/><path d="M9 21h1"/><path d="M14 3h1"/><path d="M14 21h1"/><path d="M3 9v1"/><path d="M21 9v1"/><path d="M3 14v1"/><path d="M21 14v1"/></svg>
           <span style="color: black !important; width: auto;">Area Select Tool</span>`
