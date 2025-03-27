ee18b9d353101a3cde5a85e81240f51f72fc96e1
steveoni <steohenoni2@gmail.com>
2025-03-27 17:03:53 +0100

    update

--- e2e-tests/cypress/e2e/private_teams.cy.js
+++ e2e-tests/cypress/e2e/private_teams.cy.js
@@ -53,17 +53,10 @@ describe("Create and edit team", () => {
         .click();
       cy.get("li").contains(parentOrg).click();
       cy.get("button[type=submit]").click();
-      cy.contains(`Successfully edited the ${org} team`)
+      cy.contains(`Successfully edited the ${org} team`);
       cy.visit(`/dashboard/teams/${org}/edit`).then(() => {
         cy.get("input[name=title]").should("have.value", org);
       });
-    });
-  });
-
-  it("should check if parent is private", () => {
-    cy.visit(`/dashboard/teams/${parentOrg}/edit`).then(() => {
-      cy.get("input[name=title]").should("have.value", parentOrg);
-      cy.get("button#visibility").should("contain.text", "Private");
     });
   });
 
@@ -81,28 +74,25 @@ describe("Create and edit team", () => {
       cy.get("button#visibility").click();
       cy.get("li").contains("Private").click();
       cy.get("button[type=submit]").click();
-      cy.contains(`Successfully edited the ${parentOrg} team`)
-      cy.wait(5000)
+      cy.contains(`Successfully edited the ${parentOrg} team`);
+      cy.wait(5000);
     });
   });
 
-  //it("should not be possible to view parent",
-  //  {
-  //    retries: {
-  //      runMode: 5,
-  //      openMode: 0,
-  //    },
-  //  },
-  //  () => {
-  //    cy.viewport(1600, 1000);
-  //    cy.clearCookies();
-  //    cy.clearLocalStorage();
-  //    cy.clearAllSessionStorage();
-  //    cy.visit("/");
-  //    cy.visit('/teams');
-  //    cy.contains(parentOrg).should("not.exist");
-  //    cy.contains(org).should("not.exist");
-  //  });
+  it("should check if parent is private", () => {
+    cy.login(ckanUserName, ckanUserPassword);
+    cy.visit(`/dashboard/teams/${parentOrg}/edit`).then(() => {
+      cy.get("input[name=title]").should("have.value", parentOrg);
+      cy.get("button#visibility").should("contain.text", "Private");
+    });
+  });
+
+  it("should not be possible to view parent", () => {
+    cy.login(normalUser, normalUserPassword);
+    cy.visit("/teams");
+    cy.contains(parentOrg).should("not.exist");
+    cy.contains(org).should("not.exist");
+  });
 
   it("Should edit parent team to public", () => {
     cy.login(ckanUserName, ckanUserPassword);
@@ -111,7 +101,7 @@ describe("Create and edit team", () => {
       cy.get("button#visibility").click();
       cy.get("li").contains("Public").click();
       cy.get("button[type=submit]").click();
-      cy.contains(`Successfully edited the ${parentOrg} team`)
+      cy.contains(`Successfully edited the ${parentOrg} team`);
     });
   });
 
