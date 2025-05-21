52f9a35f68100d76b425ad9c7e96d661ee378f84
Michael Polidori <contact@michaelpolidori.com>
2025-05-21 08:46:53 -0400

    Fix conflicts

--- deployment/frontend/src/components/_shared/DownloadPopup.tsx
+++ deployment/frontend/src/components/_shared/DownloadPopup.tsx
@@ -237,7 +237,6 @@ export function DownloadPopup({
                                             {subtitle}
                                         </Dialog.Title>
                                         <form
-                                            form-id="email-download"
                                             onSubmit={handleSubmit(onSubmit)}
                                             form-id="email-download"
                                             className="mt-6 text-left flex flex-col gap-y-2"
