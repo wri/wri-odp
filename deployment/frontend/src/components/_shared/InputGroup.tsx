fef2fb7311777189abbdd7fe6ba0203ef2cf385f
Luccas Mateus <luccasmmg@gmail.com>
2025-07-16 10:25:36 -0300

    Fix build

--- deployment/frontend/src/components/_shared/InputGroup.tsx
+++ deployment/frontend/src/components/_shared/InputGroup.tsx
@@ -46,24 +46,24 @@ export function InputGroup({
                                 className="h-5 w-5 text-neutral-500 ml-1 shrink-0 mt-0.5"
                                 aria-hidden="true"
                             />
-                        </DefaultTooltip>)}
-                        <DefaultTooltip
-                            content={info}
-                            contentClassName={contentClassName}
-                        >
-                            {deepInfoIcon ? (
-                                <InformationSolidCircle
-                                    className="h-5 w-5 text-neutral-500 ml-1 mb-1"
-                                    aria-hidden="true"
-                                />
-                            ) : (
-                                <InformationCircleIcon
-                                    className="h-5 w-5 text-neutral-500 ml-1 mb-1"
-                                    aria-hidden="true"
-                                />
-                            )}
                         </DefaultTooltip>
                     )}
+                    <DefaultTooltip
+                        content={info}
+                        contentClassName={contentClassName}
+                    >
+                        {deepInfoIcon ? (
+                            <InformationSolidCircle
+                                className="h-5 w-5 text-neutral-500 ml-1 mb-1"
+                                aria-hidden="true"
+                            />
+                        ) : (
+                            <InformationCircleIcon
+                                className="h-5 w-5 text-neutral-500 ml-1 mb-1"
+                                aria-hidden="true"
+                            />
+                        )}
+                    </DefaultTooltip>
                 </span>
             ) : (
                 <>{label}</>
