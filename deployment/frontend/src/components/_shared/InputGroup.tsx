8922ec1d6eb1076caa9b9aae1d156efc206ad50b
Luccas Mateus <luccasmmg@gmail.com>
2025-07-16 21:46:11 -0300

    Fix tooltip on input group

--- deployment/frontend/src/components/_shared/InputGroup.tsx
+++ deployment/frontend/src/components/_shared/InputGroup.tsx
@@ -41,29 +41,23 @@ export function InputGroup({
                     {label}{' '}
                     {required && <span className="text-red-500">*</span>}
                     {info && (
-                        <DefaultTooltip content={info} contentClassName="">
-                            <InformationCircleIcon
-                                className="h-5 w-5 text-neutral-500 ml-1 shrink-0 mt-0.5"
-                                aria-hidden="true"
-                            />
+                        <DefaultTooltip
+                            content={info}
+                            contentClassName={contentClassName}
+                        >
+                            {deepInfoIcon ? (
+                                <InformationSolidCircle
+                                    className="h-5 w-5 text-neutral-500 ml-1 mb-1"
+                                    aria-hidden="true"
+                                />
+                            ) : (
+                                <InformationCircleIcon
+                                    className="h-5 w-5 text-neutral-500 ml-1 mb-1"
+                                    aria-hidden="true"
+                                />
+                            )}
                         </DefaultTooltip>
                     )}
-                    <DefaultTooltip
-                        content={info}
-                        contentClassName={contentClassName}
-                    >
-                        {deepInfoIcon ? (
-                            <InformationSolidCircle
-                                className="h-5 w-5 text-neutral-500 ml-1 mb-1"
-                                aria-hidden="true"
-                            />
-                        ) : (
-                            <InformationCircleIcon
-                                className="h-5 w-5 text-neutral-500 ml-1 mb-1"
-                                aria-hidden="true"
-                            />
-                        )}
-                    </DefaultTooltip>
                 </span>
             ) : (
                 <>{label}</>
