8eba59b3acfbc3133019e32d02a67dc9bed7a672
Carlos Del Real <carloshdelreal@gmail.com>
2025-10-15 11:57:59 +0200

    feat: restore support e2e

--- e2e-tests/cypress/support/e2e.js
+++ e2e-tests/cypress/support/e2e.js
@@ -42,15 +42,6 @@ const apiUrl = (path) => {
 Cypress.Commands.add("login", (username, password) => {
   cy.session([username, password], () => {
     cy.on('window:before:load', (win) => {
-      // Provide a minimal Osano stub for tests so Hotjar/consent checks don't fail
-      if (!win.Osano) {
-        win.Osano = {
-          cm: {
-            getConsent: () => ({ ANALYTICS: 'GRANT' }),
-            showDrawer: () => {},
-          },
-        };
-      }
       const origAppendChild = win.document.head.appendChild;
       win.document.head.appendChild = function(el) {
         if (el.tagName === 'SCRIPT' && el.src && el.src.includes('osano')) {
