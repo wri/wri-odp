5b1137967e08b55f20a558c6a122ad1069ff9b4f
Carlos Del Real <carloshdelreal@gmail.com>
2026-03-25 21:34:45 -0500

    hotfix: he upload compatible with the default AWS bucket security settings

--- deployment/frontend/src/pages/api/sign-s3.ts
+++ deployment/frontend/src/pages/api/sign-s3.ts
@@ -46,7 +46,6 @@ export default async function handler(
                 ? `${_filePath}/${key}.${extension}`
                 : `resources/${fileHash}/${key}.${extension}`,
             ContentType: contentType as string,
-            ACL: 'public-read',
         }),
         { expiresIn: 3600 }
     );
