97bdbc9261d0223c9a9c4d820ee86135bf6b9a53
Carlos Del Real <carloshdelreal@gmail.com>
2026-04-06 10:57:48 -0500

    feat: add GFW_DATA_API_KEY (#874)
    * feat: add GFW_DATA_API_KEY
    
    * feat: improve eslint

--- deployment/frontend/src/env.mjs
+++ deployment/frontend/src/env.mjs
@@ -44,6 +44,7 @@ export const env = createEnv({
         SMTP_FROM: z.string(),
         PREFECT_INTERNAL_URL: z.string(),
         OSANO_URL: z.string(),
+        GFW_DATA_API_KEY: z.string().optional(),
     },
 
     /**
@@ -97,6 +98,7 @@ export const env = createEnv({
         NEXT_PUBLIC_HOTJAR_ID: process.env.NEXT_PUBLIC_HOTJAR_ID,
         NEXT_PUBLIC_GFW_API_KEY: process.env.NEXT_PUBLIC_GFW_API_KEY,
         OSANO_URL: process.env.OSANO_URL,
+        GFW_DATA_API_KEY: process.env.GFW_DATA_API_KEY,
         NEXT_PUBLIC_WRI_PRIVACY_POLICY_URL:
             process.env.NEXT_PUBLIC_WRI_PRIVACY_POLICY_URL ||
             'https://www.wri.org/about/privacy-policy?sitename=WRI%20Data%20Explorer&osanoid=c2a89d08-4931-4ad0-99cb-8d3aa022aaec',
--- deployment/frontend/src/pages/api/data-api-download.ts
+++ deployment/frontend/src/pages/api/data-api-download.ts
@@ -19,7 +19,13 @@ export default async function handler(
         });
     }
 
-    const apiKey = env.NEXT_PUBLIC_GFW_API_KEY;
+    const apiKey: string | undefined = env.GFW_DATA_API_KEY;
+
+    if (!apiKey) {
+        return res.status(400).json({
+            error: 'GFW_DATA_API_KEY is not set',
+        });
+    }
 
     const params = new URLSearchParams({
         grid: String(grid),
