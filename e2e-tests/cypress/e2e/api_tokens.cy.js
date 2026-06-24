92f0b6c50b78176f3d3bff6a7495fba9525ef4aa
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-06-24 19:25:14 +0300

    test_fixes

--- e2e-tests/cypress/e2e/api_tokens.cy.js
+++ e2e-tests/cypress/e2e/api_tokens.cy.js
@@ -9,6 +9,7 @@ const teamOne = `${uuid()}${Cypress.env("ORG_NAME_SUFFIX")}`;
 const adminUser = `${uuid()}${Cypress.env("USER_NAME_SUFFIX")}_admin`;
 const adminUserPassword = "test1234";
 const adminUserEmail = Math.random().toString(36).slice(2) + "@test.com";
+const tokenName = `test-token-${uuid()}`;
 
 describe("Can add and remove members from teams and topics", () => {
   before(() => {
@@ -24,27 +25,26 @@ describe("Can add and remove members from teams and topics", () => {
     cy.visit(`/dashboard/settings/edit/${adminUser}`);
     cy.contains("API Tokens", { timeout: 20000 }).click();
     cy.contains("Create API Token", { timeout: 20000 }).click();
-    cy.get('#name', { timeout: 10000 }).should('be.visible').clear().type("test-token");
+    cy.get('#name', { timeout: 10000 }).should('be.visible').clear().type(tokenName);
     cy.contains("Create Token", { timeout: 10000 }).click();
-    cy.contains("Successfully created the test-token token", { timeout: 20000 }).should('be.visible');
+    cy.contains(tokenName, { timeout: 30000 }).should('be.visible');
     cy.get("#copyButton", { timeout: 20000 }).should('be.visible').click();
-    cy.contains("Copied text to clipboard", { timeout: 20000 }).should('be.visible');
+    cy.contains("Copied", { timeout: 20000 }).should('be.visible');
   });
 
   it("List api keys", () => {
     cy.visit(`/dashboard/settings/edit/${adminUser}`);
     cy.contains("API Tokens", { timeout: 20000 }).click();
-    cy.contains("test-token", { timeout: 20000 }).should('be.visible');
+    cy.contains(tokenName, { timeout: 20000 }).should('be.visible');
   });
 
   it("Delete api keys", () => {
     cy.visit(`/dashboard/settings/edit/${adminUser}`);
     cy.contains("API Tokens", { timeout: 20000 }).click();
-    cy.contains("test-token", { timeout: 20000 }).should('be.visible');
+    cy.contains(tokenName, { timeout: 20000 }).should('be.visible');
     cy.get('.delete-token', { timeout: 20000 }).should('have.length.greaterThan', 0).first().click();
     cy.contains('Delete Token', { timeout: 10000 }).click();
-    cy.contains('Successfully deleted the test-token token', { timeout: 20000 }).should('be.visible');
-    cy.contains("test-token").should('not.exist');
+    cy.contains(tokenName, { timeout: 30000 }).should('not.exist');
   });
 
   after(() => {
