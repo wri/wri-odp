27d440c62dc3a300caa749ebfd594d60f6e76b80
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-07-01 20:31:56 +0300

    fix lint

--- deployment/frontend/src/components/dashboard/datasets/admin/datafiles/DatafileAccordion.tsx
+++ deployment/frontend/src/components/dashboard/datasets/admin/datafiles/DatafileAccordion.tsx
@@ -1,8 +1,4 @@
-import {
-    ChevronDownIcon,
-    MinusCircleIcon,
-    Squares2X2Icon,
-} from '@heroicons/react/24/outline';
+import { ChevronDownIcon, MinusCircleIcon, Squares2X2Icon } from '@heroicons/react/24/outline';
 import { Disclosure, Transition } from '@headlessui/react';
 import classNames from '@/utils/classnames';
 import { DefaultTooltip } from '@/components/_shared/Tooltip';
@@ -37,10 +33,7 @@ export function DataFileAccordion({
     }, []);
 
     return (
-        <Disclosure
-            as="div"
-            className="mx-auto w-full max-w-[1380px] px-4 sm:px-6 xxl:px-0"
-        >
+        <Disclosure as="div" className="mx-auto w-full max-w-[1380px] px-4 sm:px-6 xxl:px-0">
             {({ open }) => (
                 <>
                     <div
@@ -58,10 +51,7 @@ export function DataFileAccordion({
                                 <h3 className="text-black text-xl font-normal font-['Acumin Pro SemiCondensed'] flex items-center gap-x-2">
                                     {title}
                                     <DefaultTooltip content="Remove item">
-                                        <button
-                                            onClick={() => remove()}
-                                            aria-label="remove"
-                                        >
+                                        <button onClick={() => remove()} aria-label="remove">
                                             <MinusCircleIcon className="h-6 w-6 text-red-500" />
                                         </button>
                                     </DefaultTooltip>{' '}
@@ -74,15 +64,13 @@ export function DataFileAccordion({
                                     )}
                                     <ChevronDownIcon
                                         className={`${
-                                            open
-                                                ? 'rotate-180 transform  transition'
-                                                : ''
+                                            open ? 'rotate-180 transform  transition' : ''
                                         } h-5 w-5 text-blue-800`}
                                     />
                                 </div>
                             </Disclosure.Button>
                             <Transition
-                             as="div"
+                                as="div"
                                 enter="transition duration-100 ease-out"
                                 enterFrom="transform scale-95 opacity-0"
                                 enterTo="transform scale-100 opacity-100"
@@ -90,12 +78,7 @@ export function DataFileAccordion({
                                 leaveFrom="transform scale-100 opacity-100"
                                 leaveTo="transform scale-95 opacity-0"
                             >
-                                <Disclosure.Panel
-                                    className={classNames(
-                                        'py-5',
-                                        className ?? ''
-                                    )}
-                                >
+                                <Disclosure.Panel className={classNames('py-5', className ?? '')}>
                                     {children}
                                 </Disclosure.Panel>
                             </Transition>
