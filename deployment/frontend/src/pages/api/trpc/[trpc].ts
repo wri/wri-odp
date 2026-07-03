60aa781d70eac03b3a501efec4efa630ed23bce3
Carlos Del Real <carloshdelreal@gmail.com>
2026-07-03 11:29:47 -0500

    feat: add trcp logs to frontend

--- deployment/frontend/src/pages/api/trpc/[trpc].ts
+++ deployment/frontend/src/pages/api/trpc/[trpc].ts
@@ -1,19 +1,12 @@
 import { createNextApiHandler } from '@trpc/server/adapters/next';
 
-import { env } from '@/env.mjs';
 import { appRouter } from '@/server/api/root';
 import { createTRPCContext } from '@/server/api/trpc';
+import { logTrpcError } from '@/server/log/trpcError';
 
 // export API handler
 export default createNextApiHandler({
     router: appRouter,
     createContext: createTRPCContext,
-    onError:
-        env.NODE_ENV === 'development'
-            ? ({ path, error }) => {
-                  console.error(
-                      `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`
-                  );
-              }
-            : undefined,
+    onError: (opts) => logTrpcError(opts),
 });
