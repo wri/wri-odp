85023e96480ba1ed16e7fc8ff1d8abfe107071b7
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-07-09 16:56:04 +0300

    ODP-631-navigation_component

--- deployment/frontend/src/components/datasets-v2/Navigation/Login/index.tsx
+++ deployment/frontend/src/components/datasets-v2/Navigation/Login/index.tsx
@@ -6,7 +6,7 @@ export default function Login({ onSignIn = () => {} }: { onSignIn?: () => void }
     const [isPasswordReset, setIsPasswordReset] = useState(false);
 
     return (
-        <section className="p-6">
+        <section id="login-modal" className="p-6">
             {!isPasswordReset ? (
                 <SignInForm onSignIn={onSignIn} setIsPasswordReset={setIsPasswordReset} />
             ) : (
