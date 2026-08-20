542741c02e27f11ccd03096694ebd4dd265f633e
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-08-20 14:05:00 +0200

    remove unncesary button

--- deployment/frontend/src/components/_shared/SimpleSelect.tsx
+++ deployment/frontend/src/components/_shared/SimpleSelect.tsx
@@ -1,7 +1,6 @@
 import { Fragment } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
-import { XMarkIcon } from '@heroicons/react/24/outline';
 import classNames from '@/utils/classnames';
 import {
     Controller,
@@ -27,7 +26,6 @@ interface SimpleSelectProps<T extends FieldValues, V extends object> {
     formObj?: UseFormReturn<T>;
     name: Path<T>;
     id?: string;
-    allowClear?: boolean;
 }
 
 export default function SimpleSelect<T extends FieldValues, V extends object>({
@@ -38,7 +36,6 @@ export default function SimpleSelect<T extends FieldValues, V extends object>({
     formObj,
     name,
     id,
-    allowClear = false,
     onChange: _onChange = (val) => {},
     disabled,
     str,
@@ -63,9 +60,6 @@ export default function SimpleSelect<T extends FieldValues, V extends object>({
                 const selectedFull = options.find(
                     (o) => o.value === (str ? selected : selected?.value)
                 );
-                const hasSelection = str
-                    ? selected !== undefined && selected !== null && selected !== ''
-                    : !!selected?.value;
                 return (
                     <Listbox
                         value={selected}
@@ -79,12 +73,7 @@ export default function SimpleSelect<T extends FieldValues, V extends object>({
                     >
                         {({ open }) => (
                             <>
-                                <div
-                                    className={classNames(
-                                        'relative w-full',
-                                        maxWidth
-                                    )}
-                                >
+                                <div className={classNames('relative w-full', maxWidth)}>
                                     <Listbox.Button
                                         id={id}
                                         className={classNames(
@@ -105,18 +94,12 @@ export default function SimpleSelect<T extends FieldValues, V extends object>({
                                                 {selected &&
                                                 selectedFull &&
                                                 (str ? true : selected.label)
-                                                    ? selectedFull.label
-                                                          .charAt(0)
-                                                          .toUpperCase() +
-                                                      selectedFull.label.slice(
-                                                          1
-                                                      )
+                                                    ? selectedFull.label.charAt(0).toUpperCase() +
+                                                      selectedFull.label.slice(1)
                                                     : placeholder}
                                             </span>
-                                            {selected?.visibility ===
-                                                'private' &&
-                                                selected?.value !=
-                                                    'private' && <>&#128274;</>}
+                                            {selected?.visibility === 'private' &&
+                                                selected?.value != 'private' && <>&#128274;</>}
                                         </span>
                                         <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                                             <ChevronDownIcon
@@ -125,26 +108,6 @@ export default function SimpleSelect<T extends FieldValues, V extends object>({
                                             />
                                         </span>
                                     </Listbox.Button>
-                                    {allowClear && hasSelection && !disabled && (
-                                        <button
-                                            type="button"
-                                            className="absolute inset-y-0 right-7 flex items-center pr-1 text-gray-400 hover:text-gray-700"
-                                            onClick={(event) => {
-                                                event.preventDefault();
-                                                event.stopPropagation();
-                                                _onChange(undefined);
-                                                setSelected(
-                                                    str
-                                                        ? ('' as PathValue<T, Path<T>>)
-                                                        : (undefined as PathValue<T, Path<T>>)
-                                                );
-                                            }}
-                                            aria-label="Clear selection"
-                                            title="Clear selection"
-                                        >
-                                            <XMarkIcon className="h-4 w-4" />
-                                        </button>
-                                    )}
 
                                     <Transition
                                         show={open}
@@ -180,10 +143,8 @@ export default function SimpleSelect<T extends FieldValues, V extends object>({
                                                             >
                                                                 {option.label}
                                                                 {option?.visibility &&
-                                                                option.visibility ===
-                                                                    'private' &&
-                                                                option.value !=
-                                                                    'private' ? (
+                                                                option.visibility === 'private' &&
+                                                                option.value != 'private' ? (
                                                                     <span className="ml-1">
                                                                         {' '}
                                                                         &#128274;
